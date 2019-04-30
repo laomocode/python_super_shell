46d0b2606472246d6eff41e833756e1be6dab460
laomocode <3344907598@qq.com>
2019-04-30 16:23:24 +0800

    加入了极其简陋的计算器功能。

--- terminal/python终端.py
+++ terminal/python终端.py
@@ -6,11 +6,28 @@ import time
 def 天气():
     if 系统=="posix":
         print(os.system("curl wttr.in"))
+def 计算器():
+    while 1:
+        选项=int(input("请输入你要的选项（1）加法计算，（2）减法计算，（3）乘法计算，（4）除法计算，（5）退出："))
+        if 选项==5:
+            break
+        因数1=int(input("请输入第一个因数："))
+        因数2=int(input("请输入第二个因数：")) 
+        if 选项==1:
+            结果=因数1+因数2
+        elif 选项==2:
+            结果=因数1-因数2
+        elif 选项==3:
+            结果=因数1*因数2
+        elif 选项==4:
+            结果=因数1/因数2
+        print(结果)
 def 帮助():
     print("帮助：")
     print("输入“退出”来退出终端。")
     print("输入“次数”显示输入次数。")
     print("输入“系统”查看系统信息。")
+    print("输入“计算器”计算算式。")
     if 系统=="posix":
         print("输入“天气”查看当前天气（为英文）。")
 def 系统信息(形式):
@@ -40,6 +57,9 @@ while 1:
         continue
     elif 输入=="系统":
         print(系统信息("普通"))
+        continue
+    elif 输入=="计算器":
+        计算器()
         continue
     输出=os.system(输入)
     print(输出)
